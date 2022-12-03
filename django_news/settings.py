6928b12b1536c206acfaf5a7d9bc2ee75dda124a
unknown <inc.africa.tz@gmail.com>
2022-12-03 13:44:28 -0800

    5 commit

--- django_news/settings.py
+++ django_news/settings.py
@@ -120,9 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / "static",
-    ]
+
 
 #MEDIA
 MEDIA_URL = 'media/'
@@ -132,14 +130,10 @@ MEDIA_ROOT = BASE_DIR / "media/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login'
 
-
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
